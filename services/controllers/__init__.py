b80b0829863b51cefbc01f9b9ce21d6a3f1f411f
fernandagitai <fernandaherculanom@gmail.com>
2024-03-04 23:40:25 -0300

    feat: crud notícias

--- services/controllers/__init__.py
+++ services/controllers/__init__.py
@@ -2,3 +2,4 @@ from .form_question_controller import FormQuestionAPIView, FormItemAPIView
 from .form_controller import FormAPIView
 from .form_answer_controller import AnswerFormAPIView
 from .tutorial_controller import TutorialAPIView
+from .news_controller import NewsAPIView
--- services/routes/__init__.py
+++ services/routes/__init__.py
@@ -1,7 +1,9 @@
 from .form_routes import urlpatterns as form_routes
 from .tutorial_routes import urlpatterns as tutorial_routes
+from .news_routes import urlpatterns as news_routes
 
 urlpatterns = [
     *form_routes,
     *tutorial_routes,
+    *news_routes,
 ]

c3a89eb85391d02de10bf995851dd2c0b08798d8
Lucas Lemos <llcf@ic.ufal.br>
2024-03-11 07:53:07 -0300

    feat: add corsheaders

--- academic_backend/settings.py
+++ academic_backend/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-
+CORS_ALLOW_ALL_ORIGINS  = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,9 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "services",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",

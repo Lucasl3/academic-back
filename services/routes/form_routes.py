166b4e39996e28a68fa57841d88bc74bc33f1b43
fernandagitai <fernandaherculanom@gmail.com>
2024-03-13 21:08:40 -0300

    fix: acessando formulario com mais detalhes

--- services/routes/form_routes.py
+++ services/routes/form_routes.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from services.controllers import FormQuestionAPIView, FormItemAPIView, FormAPIView, AnswerFormAPIView
+from services.controllers import (
+    FormQuestionAPIView, 
+    FormItemAPIView, 
+    FormAPIView, 
+    AnswerFormAPIView
+    )
 
 # Definir rotas de acordo com o front-end
 urlpatterns = [
     path('form-question/', FormQuestionAPIView.as_view()),
     path('form-item/', FormItemAPIView.as_view()),
     path('form/', FormAPIView.as_view()),
+    path('form/<int:co_form>/', FormAPIView.as_view()),
     path('answer-form/', AnswerFormAPIView.as_view()),
 ]
